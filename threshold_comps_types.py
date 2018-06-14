79659607b7beed7c5186199ba9d1dc9526e9c1ae
lerner.67 <lerner98@gmail.com>
2018-06-14 11:37:28 -0400

    updated threshold components display. slow but functional

--- threshold_comps_types.py
+++ threshold_comps_types.py
@@ -37,7 +37,7 @@ def get_img_and_mask():
     img = Image.open(img_path)
     img_arr = np.array(img)
     img_arr = cv.cvtColor(img_arr, cv.COLOR_BGR2GRAY)
-    return img_id, img, img_arr, len(masks), total_mask 
+    return img_id, img_path, img, img_arr, len(masks), total_mask 
 
 
 """ Return the number of connected components and the label mask for an image """
@@ -47,34 +47,52 @@ def conn_comp(img):
 
 
 
+img_arr, img_thresh = None, None
+num_labels, thresh_labels = None, None
+thresh_disp, thresh_comp_disp = None, None
+num_labels_true, labels_true = None, None
+
 """ Initial loading and plotting """
-img_id, img, img_arr, num_mask, total_mask = get_img_and_mask()
-num_labels, labels = conn_comp(total_mask)
+def load_img():
+    global img_arr, img_thresh, num_labels, thresh_labels, thresh_disp
+    global thresh_comp_disp, num_labels_true, labels_true
 
-""" Plot the ground truth total mask and its connected components """
-axs[0,0].imshow(total_mask, cmap='gray')
-axs[0,1].imshow(labels, cmap='jet')
+    img_id, img_path, img, img_arr, num_mask, total_mask = get_img_and_mask()
+    num_labels_true, labels_true = conn_comp(total_mask)
 
-print 'Number of masks: %d' % num_mask
-print 'Number of components in original: %d' % num_labels
-num_labels_true = num_labels
+    """ Plot the ground truth total mask and its connected components """
+    axs[0,0].imshow(total_mask, cmap='gray')
+    axs[0,1].imshow(labels_true, cmap='jet')
 
-""" Initially threshold the input image and find its connected components """
-_, img_thresh = cv.threshold(img_arr, 25, 255, cv.THRESH_BINARY)
-num_labels, thresh_labels = conn_comp(img_thresh)
+    print 'Number of masks: %d' % num_mask
+    print 'Number of components in original: %d' % num_labels_true
 
-""" Plot the thresholded mask and its connected components """
-thresh_disp = axs[1,0].imshow(img_thresh, cmap='gray')
-thresh_comp_disp = axs[1,1].imshow(thresh_labels, cmap='jet')
+    """ Initially threshold the input image and find its connected components """
+    _, img_thresh = cv.threshold(img_arr, 25, 255, cv.THRESH_BINARY)
+    num_labels, thresh_labels = conn_comp(img_thresh)
 
-print 'Number of components in thresholded: %d' % num_labels
+    """ Plot the thresholded mask and its connected components """
+    global thresh_disp, thresh_comp_disp
+    thresh_disp = axs[1,0].imshow(img_thresh, cmap='gray')
+    thresh_comp_disp = axs[1,1].imshow(thresh_labels, cmap='jet')
+
+    print 'Number of components in thresholded: %d' % num_labels
+
+    """ If no connect components were found in the thresholded image,
+    display the original image to see what might've caused this """
+    if num_labels <= 1:
+        Image.open(img_path).show()
 
 
 
 """ Change the thresold value and possible threshold type for the input image """
-def change_thresh(thresh_val, ttype=cv.THRESH_BINARY):
+ttype=cv.THRESH_BINARY
+def change_thresh(thresh_val):
+    global img_arr, img_thresh, num_labels, thresh_labels, ttype
     _, img_thresh = cv.threshold(img_arr, thresh_val, 255, ttype)
     num_labels, thresh_labels = conn_comp(img_thresh)
+
+    global thresh_disp, thresh_comp_disp
     thresh_disp.set_data(img_thresh)
     thresh_comp_disp.set_data(thresh_labels)
     print 'Threshold: %d; %d/%d' % (thresh_val, num_labels, num_labels_true)
@@ -88,11 +106,19 @@ thresh_slider.on_changed(change_thresh)
 
 """ Change the threshold type to BINARY """
 def thresh_bin(event):
-    change_thresh(thresh_slider.val, ttype=cv.THRESH_BINARY)
+    global ttype
+    ttype = cv.THRESH_BINARY
+    change_thresh(thresh_slider.val)
 
 """ Change the threshold type to TOZERO """
 def thresh_tz(event):
-    change_thresh(thresh_slider.val, ttype=cv.THRESH_TOZERO)
+    global ttype
+    ttype = cv.THRESH_TOZERO
+    change_thresh(thresh_slider.val)
+
+""" Get a new image and repeat the display process """
+def new_img(event):
+    load_img() 
 
 
 """ Create two buttons to change the threshold type """
@@ -104,8 +130,11 @@ ax_tz = plt.axes([0.35, 0.925, 0.15, 0.03])
 btz = Button(ax_tz, 'To Zero')
 btz.on_clicked(thresh_tz)
 
-if num_labels <= 1:
-    Image.open(img_path).show()
+ax_new = plt.axes([0.55, 0.925, 0.15, 0.03])
+bnew = Button(ax_new, 'New Image')
+bnew.on_clicked(new_img)
 
+
+load_img()
 plt.show()
 
